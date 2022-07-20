98341d64d4cee5823f66517128f9344e037062a0
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-07-20 14:42:07 +0000

    of CORS!

--- src/server.py
+++ src/server.py
@@ -10,6 +10,9 @@ import ujson
 from sanic import Sanic, Request, json, text
 from sanic_ext import Config
 
+from cors import add_cors_headers
+from options import setup_options
+
 from chess_bp import chess_blueprint as chessBp
 from login import login
 from misc import misc
@@ -77,6 +80,13 @@ async def index(request: Request):
     )
 
     return text(resp)
+
+
+# Add OPTIONS handlers to any route that is missing it
+app.register_listener(setup_options, "before_server_start")
+
+# Fill in CORS headers
+app.register_middleware(add_cors_headers, "response")
 
 if __name__ == '__main__':
     app.run(
