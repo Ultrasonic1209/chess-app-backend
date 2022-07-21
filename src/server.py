99e017a98a281cd92e6ccc48afeb4aad5c5ec3e5
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-07-21 17:44:00 +0000

    cf

--- src/server.py
+++ src/server.py
@@ -81,7 +81,7 @@ async def add_cors_response(request: Request, response: sanic.response.HTTPRespo
     """
     if ((request.method.upper() != "OPTIONS") and
         (response.headers.get("Access-Control-Allow-Origin") is None) and
-        (app.config.CORS_ORIGINS.match(request.headers["Origin"]))):
+        (app.config.CORS_ORIGINS.match(request.headers.get("Origin")))):
         response.headers["Access-Control-Allow-Origin"] = request.headers["Origin"]
 
         return response
