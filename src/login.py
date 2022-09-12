f7119822ffcea94af80980091a0e268da878983c
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-09-12 17:37:56 +0000

    h

--- src/login.py
+++ src/login.py
@@ -188,7 +188,7 @@ async def do_login(request: Request, params: LoginBody, user: models.User, sessi
     response.cookies[".CHECKMATESECRET"] = token
     response.cookies[".CHECKMATESECRET"]["secure"] = True
     response.cookies[".CHECKMATESECRET"]["samesite"] = "None"
-    response.cookies[".CHECKMATESECRET"]["domain"] = get_hostname(request.headers.get("origin", ""))
+    response.cookies[".CHECKMATESECRET"]["domain"] = "." + get_hostname(request.headers.get("origin", ""))
     response.cookies[".CHECKMATESECRET"]["comment"] = "I'm in so much pain"
 
     if params.rememberMe:
